ba70a2b56f33bbad0d619e21254af913df63b2d3
n1c00o <34602094+n1c00o@users.noreply.github.com>
2020-10-19 20:34:58 +0200

    fixed some build bugs and removed .env. Now use flags

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,5 @@ WORKDIR /build
 RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -ldflags '-extldflags "-static"' -o main .
 FROM alpine:latest
 COPY --from=builder /build/main /app/
-COPY --from=builder /build/.env /app/
 WORKDIR /app
 CMD ["./main"]
--- main.go
+++ main.go
@@ -1,26 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"math/rand"
 	"os"
 	"os/signal"
-	"strconv"
+	"strings"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/joho/godotenv"
 )
 
 var (
-	legit  = 0
-	legitv int
+	channel string
+	token string
 )
 
+func init() {
+	flag.StringVar(&channel, "channel", "INVALID", "--channel <CHANNEL ID>")
+	flag.StringVar(&token, "token", "INVALID", "--token <USER TOKEN !PRIVATE!>")
+	flag.Parse()
+}
+
 func main() {
-	_ = godotenv.Load()
 	// Create a new Discord session using the provided bot token.
-	dg, err := discordgo.New(os.Getenv("DISCORD_USER_TOKEN"))
-	legitv, _ = strconv.Atoi(os.Getenv("LEGIT_AT"))
+	dg, err := discordgo.New(token)
 	if err != nil {
 		fmt.Println("Error creating Discord session: ", err)
 		return
@@ -51,30 +57,38 @@ func main() {
 	_ = dg.Close()
 }
 
-func ready(_ *discordgo.Session, _ *discordgo.Ready) {
+func ready(s *discordgo.Session, _ *discordgo.Ready) {
 	fmt.Println("Automation ready!  Press CTRL-C to exit.")
-	fmt.Println("Channel: " + os.Getenv("DISCORD_CHANNEL_ID"))
+	c, err := s.Channel(channel)
+	if err != nil {
+		fmt.Println("Cannot find channel with ID " + channel + "->\n" + err.Error())
+	} else {
+		fmt.Println("Channel: #" + c.Name + " with ID " + c.ID)
+	}
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-	if os.Getenv("DISCORD_CHANNEL_ID") == m.ChannelID {
+	if channel == m.ChannelID {
 		if m.Author.ID == "755580145078632508" && m.Embeds[0].Title == "A trick-or-treater has stopped by!" {
 			c, _ := s.Channel(m.ChannelID)
 			fmt.Println("Trick Or Treat in Channel #" + c.Name)
-			if legit == 0 {
+			if strings.Contains(m.Embeds[0].Description, "h!trick") {
+				_,_ = s.ChannelMessageSend(m.ChannelID, "h!trick")
+				fmt.Println("Sent `h!trick`")
+				seemsLegit(s, m)
+			} else if strings.Contains(m.Embeds[0].Description, "h!treat") {
 				_, _ = s.ChannelMessageSend(m.ChannelID, "h!treat")
-				fmt.Println("Got Trick Or Treat")
-
-				legit++
+				fmt.Println("Sent `h!treat`")
+				seemsLegit(s, m)
 			} else {
-				fmt.Println("LEGIT! Lost Trick Or Treat")
-				legit++
-				if legit == legitv {
-					legit = 0
-				}
+				fmt.Println("Unknown, did nothing...")
 			}
 		}
 	}
+}
 
+func seemsLegit(s *discordgo.Session, m *discordgo.MessageCreate) {
+	time.Sleep(1 * time.Second)
+	ans := [5]string{"Yeah!", "Boooom", "!!!!!", "fast af", "yes"}
+	_, _ = s.ChannelMessageSend(m.ChannelID, ans[rand.Intn(len(ans))])
 }
